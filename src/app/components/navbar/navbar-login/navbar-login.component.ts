64c7dc9e54d685079a4aebe1c9901bec9f035083
xFNighTMaRe <brodriguezserrano43@gmail.com>
2020-12-21 15:05:17 +0100

    nothing

--- src/app/components/navbar/navbar-login/navbar-login.component.ts
+++ src/app/components/navbar/navbar-login/navbar-login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-login.component.scss']
 })
 export class NavbarLoginComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public show() {
+    const hide:any = document.getElementsByClassName("hide");
+    for(let i=0; i<hide.length;i++) {
+      if(hide[i].style.display === "none") {
+        hide[i].style.display="flex"
+      } else {
+        hide[i].style.display="none"
+    }}
+  }
 }
