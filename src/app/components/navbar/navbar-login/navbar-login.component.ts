33153f64a7390b9c56a93d71e6478413fda4eaf2
xFNighTMaRe <brodriguezserrano43@gmail.com>
2020-12-23 15:45:11 +0100

    apiRest

--- src/app/components/navbar/navbar-login/navbar-login.component.ts
+++ src/app/components/navbar/navbar-login/navbar-login.component.ts
@@ -10,13 +10,4 @@ export class NavbarLoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  public show() {
-    const hide:any = document.getElementsByClassName("hide");
-    for(let i=0; i<hide.length;i++) {
-      if(hide[i].style.display === "none") {
-        hide[i].style.display="flex"
-      } else {
-        hide[i].style.display="none"
-    }}
-  }
 }
