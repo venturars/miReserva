9fea1b03f907a03d426eecdf4802ea6e8d309031
carolly3024 <carolly.garcia@gmail.com>
2020-12-27 12:12:16 +0100

    rama carolly11

--- src/app/models/user-owner.ts
+++ src/app/models/user-owner.ts
@@ -1,8 +1,9 @@
 export class UserOwner {
-    constructor(public owner_id:number,
-                public cif:string,
-                public name:string,
-                public surname:string,
-                public photo:string
+    constructor(
+        public owner_id:number,
+        public cif:string,
+        public name:string,
+        public surname:string,
+        public photo:string
         ){}
 }
--- src/app/shared/service-login.service.ts
+++ src/app/shared/service-login.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceLoginService {
-
-  constructor() { }
+  private url:string = "http://localhost:3000/login";
+  /* public userCustomer =  */
+  constructor(
+    private http:HttpClient
+  ) { }
+  public getUsers(
+    mail:string,
+    password:string
+  ) {
+    return this.http.post(this.url, {
+      "mail": mail,
+      "password": password
+    })
+  }
 }
