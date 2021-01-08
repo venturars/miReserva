dab82bd0ae388d69f6fc46990f8ad391b0fbba53
jesuscano80 <jesuscano@yahoo.es>
2021-01-08 10:29:24 +0100

    jesus 20 2

--- src/app/components/maprest/maprest.component.ts
+++ src/app/components/maprest/maprest.component.ts
@@ -26,7 +26,7 @@ export class MaprestComponent implements OnInit {
 
     this.marker= new Marker([this.apiRestaurants.restaurant.latitude ,this.apiRestaurants.restaurant.longitude]).addTo(this.map);
           this.marker.bindPopup(this.apiRestaurants.restaurant.name+"<br>"+this.apiRestaurants.restaurant.street_name+" , "+ this.apiRestaurants.restaurant.street_number)
-          .on('click', function(){console.log(this)}, this).openPopup()
+          
 
   }
 
