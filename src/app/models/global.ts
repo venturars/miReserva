03ed759c5effbe3586fbf82793b4183f09d9a3b3
jesuscano80 <jesuscano@yahoo.es>
2021-02-05 16:42:03 +0100

    cambio global

--- src/app/models/global.ts
+++ src/app/models/global.ts
@@ -1,5 +1,5 @@
 export class Global {
-
+    PORT= process.env.PORT || 3000;
     // public url:string = "http://localhost:3000";
     public url:any= process.env.PORT || 3000;
     constructor() { }
