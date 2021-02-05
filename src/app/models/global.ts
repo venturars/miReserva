892170668e50164ca98067b21e6b9afca96de371
jesuscano80 <jesuscano@yahoo.es>
2021-02-05 16:34:53 +0100

    cambio heroku global class

--- src/app/models/global.ts
+++ src/app/models/global.ts
@@ -1,6 +1,6 @@
 export class Global {
 
-    public url:string = "http://localhost:3000";
-
+    // public url:string = "http://localhost:3000";
+    public url:any= process.env.PORT || 3000;
     constructor() { }
 }
