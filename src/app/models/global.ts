cdb8fbf71770560260047594a3522b58f62dae16
jesuscano80 <jesuscano@yahoo.es>
2021-02-06 14:33:45 +0100

    poniendo clase global

--- src/app/models/global.ts
+++ src/app/models/global.ts
@@ -3,11 +3,12 @@ import { Router } from "@angular/router";
 import {Port} from "../../../apirest.js";
 export class Global {
     
-    public url:string = (Port==3000) ? "http://localhost:3000" : window.location.href;
+    public url:string = window.location.href;
     
     
     constructor() {
         
-        console.log("hola");
+        console.log(Port);
+        console.log(Port.PORT);
      }
 }
