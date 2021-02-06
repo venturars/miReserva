d55f00c2ac7ff6e97ce2649f9f66f301fef57095
jesuscano80 <jesuscano@yahoo.es>
2021-02-06 18:18:12 +0100

    poniendo clase global

--- apirest.js
+++ apirest.js
@@ -1705,4 +1705,3 @@ app.listen(PORT, () => {
     console.log("listening to port"+ PORT);
 });
 
-export let puerto="hola" + PORT;
--- src/app/models/global.ts
+++ src/app/models/global.ts
@@ -1,15 +1,12 @@
-import { Router } from "@angular/router";
-import {puerto} from "apirest.js";
-// import {PORT} from "apirest.js";
+
 export class Global {
     
     
     public url:string = window.location.href;
     
     constructor() {
-        console.log(puerto);
-           // console.log(Port);
-        // console.log(PORT);
+        
+ 
      }
 
    
