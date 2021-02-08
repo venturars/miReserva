6ff65f06569010cb721b86c268c5aaa0b8b5e5f8
jesuscano80 <jesuscano@yahoo.es>
2021-02-08 14:34:51 +0100

    cambiando disconnect

--- apirest.js
+++ apirest.js
@@ -36,8 +36,15 @@ function handleDisconnect() {
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log("ESTAMOS AQUI") // Connection to the MySQL server is usually
+      connection.connect((err)=>{
+        if (err)
+            console.log(err);
+            else
+            console.log("Conectado");
+
+      });                        // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
     }
